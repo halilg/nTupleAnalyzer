9cfb4b99f457aa19e2d3dbff3af5af23db062633
Halil Gamsizkan <halilgamsizkan@gmail.com>
2016-04-29 15:25:59 +0300

    small fix

--- analyze_test.zsh
+++ analyze_test.zsh
@@ -3,4 +3,8 @@
 PT=41
 ELEID=0
 
-time ./nTupleAnalyzer.exe analyze_st.cfg analysis:cut_ele_PT=$PT framework:maxEvents=5 analysis:cut_ele_ID=$ELEID framework:outRootFile=root/pt${PT}-id${ELEID}-nTuple_ST_multi-analysis.root
+time ./nTupleAnalyzer.exe analyze_st.cfg analysis:cut_ele_PT=$PT \
+                                         framework:maxEvents=5 \
+                                         analysis:cut_ele_ID=$ELEID \
+                                         framework:outRootFile=root/pt${PT}-id${ELEID}-nTuple_ST_multi-analysis.root \
+                                         framework:outJSONFile=results/pt${PT}-id${ELEID}-nTuple_ST_multi-analysis.json
